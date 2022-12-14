b38325e9f400c8df0755482375379aaa07ff97a1
Deyan Terziyski <98098513+deyanTerz@users.noreply.github.com>
2022-12-14 16:52:17 +0200

    Add files via upload

---
+++ GridAutoRowBugDemo/GridAutoRowBugDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GridAutoRowBugDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ GridAutoRowBugDemo/GridAutoRowBugDemo/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace GridAutoRowBugDemo;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    private void Button_Clicked(object sender, System.EventArgs e)
+    {
+        Button button = (Button)sender;
+        button.HeightRequest = button.HeightRequest == 100 ? 50 : 100;
+    }
+}
+
